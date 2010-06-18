584089b02f1eb1cecfe3c27df76372c2234fd492
mpl <mpl@localhost>
2010-06-19 01:09:49 +0200

    gen thumbs, add random

--- gogallery.go
+++ gogallery.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"syscall"
 	"sort"
 	"path"
 	"fmt"
@@ -10,6 +11,7 @@ import (
 	"http"
 	"regexp"
 	"template"
+	"rand"
 //	"strings"
 	
 	sqlite "gosqlite.googlecode.com/hg/sqlite"
@@ -17,10 +19,11 @@ import (
 
 const maxDirDepth = 24
 const templDir = "tmpl/"
+const thumbsDir = ".thumbs"
 const picpattern = "/pic/"
 const tagpattern = "/tag/"
 const tagspattern = "/tags"
-const thumbsdir = ".thumbs"
+const randompattern = "/random"
 
 var (
 	rootDir, _ = os.Getwd();
@@ -30,6 +33,7 @@ var (
 	titleValidator = regexp.MustCompile("^[a-zA-Z0-9]+$")
 	templates = make(map[string]*template.Template)
 	picValidator = regexp.MustCompile(".*(jpg|JPG|jpeg|JPEG|png)$")
+	maxId = 1
 
 	// command flags 
     snapsize   = flag.Int("snapsize", 0, "height of the thumbnail pictures")
@@ -59,7 +63,8 @@ func (p *lines) Write(line string) (n int, err os.Error) {
 
 type page struct {
 	title	string
-	body	lines
+	pics	lines
+	thumbs	lines
 }
 
 // http 
@@ -73,21 +78,25 @@ func renderTemplate(c *http.Conn, tmpl string, p *page) {
 
 func tagPage(tag string) (*page, os.Error) {
 	stmt, err := db.Prepare(
-		"SELECT file FROM tags where tag = '" + tag + "'")
+		"select file from tags where tag = '" + tag + "'")
 	errchk(err)
 	
 	var t string
-	var body lines
+	var pics lines
+	var thumbs lines
 	errchk(stmt.Exec())
 	for stmt.Next() {
 		errchk(stmt.Scan(&t))
-		body.Write(t)
+		pics.Write(t)
+		dir, file := path.Split(t)
+		thumb := path.Join(dir, thumbsDir, file)
+		thumbs.Write(thumb)
 	}
 	title := tag
 	
 	stmt.Finalize()
 
-	return &page{title: title, body: body}, nil
+	return &page{title: title, pics: pics, thumbs: thumbs}, nil
 }
 
 //TODO: tags size
@@ -98,17 +107,17 @@ func tagsPage() (*page, os.Error) {
 	
 	var s string
 	var i int
-	var body lines
+	var pics lines
 	errchk(stmt.Exec())
 	for stmt.Next() {
 		errchk(stmt.Scan(&s, &i))
-		body.Write(s)
+		pics.Write(s)
 	}
 	title := "All tags"
 	
 	stmt.Finalize()
 
-	return &page{title: title, body: body}, nil
+	return &page{title: title, pics: pics, thumbs: nil}, nil
 }
 
 func tagHandler(c *http.Conn, r *http.Request, urlpath string) {
@@ -128,7 +137,8 @@ func tagHandler(c *http.Conn, r *http.Request, urlpath string) {
 func picHandler(c *http.Conn, r *http.Request, urlpath string) {
 	var p page
 	p.title = urlpath[len(picpattern):]
-	p.body = nil
+	p.pics = nil
+	p.thumbs = nil
 	err := r.ParseForm()
 	if err != nil {
 		http.Error(c, err.String(), http.StatusInternalServerError)
@@ -141,7 +151,9 @@ func picHandler(c *http.Conn, r *http.Request, urlpath string) {
 			// only allow single alphanumeric word tag 
 			if titleValidator.MatchString(newtag) {
 				errchk(db.Exec(
-					"INSERT INTO tags VALUES ('" + p.title + "', '" + newtag + "')"))
+				"insert into tags values (" + 
+					fmt.Sprint(maxId) + ", '" + p.title + "', '" + newtag + "')"))
+				maxId++;
 			}
 			break
     	}
@@ -156,6 +168,23 @@ func tagsHandler(c *http.Conn, r *http.Request, urlpath string) {
 		return
 	}
 	renderTemplate(c, "tags", p)
+}
+
+//TODO: set maxId when initDb has not been run
+func randomHandler(c *http.Conn, r *http.Request, urlpath string) {
+	randId := rand.Intn(maxId) + 1
+	stmt, err := db.Prepare(
+		"select file from tags where id = " + fmt.Sprint(randId))
+	errchk(err)
+
+	var s string
+	errchk(stmt.Exec())
+	if stmt.Next() {
+		errchk(stmt.Scan(&s))
+	}
+	stmt.Finalize()
+	s = picpattern + s
+	http.Redirect(c, s, http.StatusFound)
 }
 
 func serveFile(c *http.Conn, r *http.Request) {
@@ -174,9 +203,8 @@ func initDb() {
 	var err os.Error
 	db, err = sqlite.Open(*dbfile)
 	errchk(err)
-	db.Exec("DROP TABLE tags")
-	errchk(db.Exec("CREATE TABLE tags (file text, tag text)"))
-//	err = scanDir(*picsdir)
+	db.Exec("drop table tags")
+	errchk(db.Exec("create table tags (id int, file text, tag text)"))
 	err = scanDir(*picsdir)
 	if err != nil {
 		log.Exit(err)
@@ -202,20 +230,33 @@ func scanDir(dirpath string) os.Error {
 	}
 	currentDir.Close()		
 	sort.SortStrings(names)
+	e := 0
+	e = syscall.Mkdir(path.Join(dirpath, thumbsDir), 0755) 
+	if e != 0 && e != syscall.EEXIST {
+		return os.Errno(e)
+	}
 	for _,v := range names {
 		childpath := path.Join(dirpath, v)
 		fi, err := os.Lstat(childpath)
 		if err != nil {
 			return err
 		}
-		if fi.IsDirectory() {
-			scanDir(childpath)
+		if fi.IsDirectory() && v != thumbsDir {
+			err = scanDir(childpath)
+			if err != nil {
+				return err
+			}
 		} else {
 			if picValidator.MatchString(childpath) {
-//				mkThumb(childpath)
+				err = mkThumb(childpath)
+				if err != nil {
+					return err
+				}
 				path := childpath[rootDirLen+1:]
 				errchk(db.Exec(
-					"INSERT INTO tags VALUES ('" + path + "', 'all')"))
+					"insert into tags values (" +
+						fmt.Sprint(maxId) + ", '" + path + "', 'all')"))
+				maxId++;
 			}
 		}
 
@@ -223,21 +264,34 @@ func scanDir(dirpath string) os.Error {
 	return err
 }
 
-/*
-func mkThumb(filepath string) {
+func mkThumb(filepath string) os.Error {
 	dir, file := path.Split(filepath)
-	thumb := path.Join(dir, thumbsdir, file)
-	//TODO: don't do it if file exists
-	var args2 []string = make([]string, 2)
-	args[0] = path.Join("/usr/bin/convert")
-	args[1] = fullpath
+	thumb := path.Join(dir, thumbsDir, file)
+	fd, err := os.Open(thumb, os.O_CREAT|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		if err != os.EEXIST {
+			return err
+		}
+		return nil
+	}
+	fd.Close()
+	var args []string = make([]string, 5)
+	args[0] = "/usr/bin/convert"
+	args[1] = filepath
 	args[2] = "-thumbnail"
 	args[3] = "200x300"
-	args[4] = "200x300"
+	args[4] = thumb
 	fds := []*os.File{os.Stdin, os.Stdout, os.Stderr}
-	os.ForkExec(args[0], args, os.Environ(), "", fds)
+	pid, err := os.ForkExec(args[0], args, os.Environ(), "", fds)
+	if err != nil {
+		return err
+	}
+	_, err = os.Wait(pid, os.WNOHANG)
+	if err != nil {
+		return err
+	}
+	return nil
 }
-*/
 
 func init() {
 	for _, tmpl := range []string{"tag", "pic", "tags"} {
@@ -272,6 +326,7 @@ func main() {
 	http.HandleFunc(tagpattern, makeHandler(tagHandler))
 	http.HandleFunc(picpattern, makeHandler(picHandler))
 	http.HandleFunc(tagspattern, makeHandler(tagsHandler))
+	http.HandleFunc(randompattern, makeHandler(randomHandler))
 	http.HandleFunc("/", http.HandlerFunc(serveFile))
 	http.ListenAndServe(":8080", nil)
 }
