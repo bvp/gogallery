14eff4a50e10b27c17d003c699d00046ba401dd4
mpl <mpl@localhost>
2010-07-18 23:27:50 +0200

    uses mime multipart now :)

--- http.go
+++ http.go
@@ -11,7 +11,7 @@ import (
 	"io/ioutil"
 	"bytes"
 	"time"
-//	"log"
+//	"fmt"
 )
 
 const maxupload int = 2e6
@@ -21,6 +21,8 @@ var (
 	titleValidator = regexp.MustCompile("^[a-zA-Z0-9]+$")
 	picValidator = regexp.MustCompile(".*(jpg|JPG|jpeg|JPEG|png|gif|GIF)$")
 	templates = make(map[string]*template.Template)
+	fileinform = "upload"
+	taginform = "tag"
 )
 
 type lines []string
@@ -55,120 +57,6 @@ type page struct {
 func newPage(title string, body lines) *page {
 	p := page{title, *host, body, picpattern, tagspattern, tagpattern, uploadpattern}
 	return &p
-}
-
-type Request2 struct {
-	*http.Request
-	upload []byte
-}
-
-func NewRequest2(r *http.Request, upload []byte) *Request2 {
-    return &Request2{r, upload}
-}
-
-// ParseForm parses the request body as a form for POST requests, or the raw query for GET requests.
-// It is idempotent.
-func (r *Request2) ParseForm() (err os.Error) {
-    if r.Form != nil {
-        return
-    }
-
-    var query string
-    switch r.Method {
-    case "GET":
-        query = r.URL.RawQuery
-    case "POST":
-        if r.Body == nil {
-            r.Form = make(map[string][]string)
-            return os.ErrorString("missing form body")
-        }
-        ct := r.Header["Content-Type"]
-        switch strings.Split(ct, ";", 2)[0] {
-        case "text/plain", "application/x-www-form-urlencoded", "":
-            var b []byte
-            if b, err = ioutil.ReadAll(r.Body); err != nil {
-                r.Form = make(map[string][]string)
-                return err
-            }
-            query = string(b)
-       case "multipart/form-data":
-            boundary := strings.Split(ct, "boundary=", 2)[1]
-            var b []byte
-            if b, err = ioutil.ReadAll(r.Body); err != nil {
-                return err
-            }
-            parts := bytes.Split(b, []byte("--"+boundary+"--\r\n"), 0)
-            parts = bytes.Split(parts[0], []byte("--"+boundary+"\r\n"), 0)
-            for _, data := range parts {
-                if len(data) < 2 {
-                    continue
-                }
-                data = data[0 : len(data)-2] // remove the \r\n
-                var line []byte
-                var rest = data
-                //content-disposition params
-                cdparams := map[string]string{}
-                for {
-                    res := bytes.Split(rest, []byte{'\r', '\n'}, 2)
-                    if len(res) != 2 {
-                        break
-                    }
-                    line = res[0]
-                    rest = res[1]
-                    if len(line) == 0 {
-                        break
-                    }
-
-                    header := strings.Split(string(line), ":", 2)
-                    n := strings.TrimSpace(header[0])
-                    v := strings.TrimSpace(header[1])
-                    if n == "Content-Disposition" {
-                        cdparts := strings.Split(v, ";", 0)
-                        for _, cdparam := range cdparts[1:] {
-                            split := strings.Split(cdparam, "=", 2)
-                            pname := strings.TrimSpace(split[0])
-                            pval := strings.TrimSpace(split[1])
-                            cdparams[pname] = pval
-                        }
-                    }
-                }
-                //if the param doesn't have a name, ignore it
-                if _, ok := cdparams["name"]; !ok {
-                    continue
-                }
-                name := cdparams["name"]
-                //check if name is quoted
-                if strings.HasPrefix(name, `"`) {
-                    name = name[1 : len(name)-1]
-                }
-                // if it's a file, store it in the upload member,
-				// and add the filename to the query
-                if filename, ok := cdparams["filename"]; ok {
-                    if strings.HasPrefix(filename, `"`) {
-                        filename = filename[1 : len(filename)-1]
-                    }
-					if len(rest) > maxupload {
-						err = os.NewError("upload too large")
-						return err
-					}
-					copy(r.upload, rest)
-					r.upload = r.upload[0:len(rest)]
-					query += "&upload=" + filename
-					continue
-                }
-				// if it's the tag, add it to the query
-				if name == "tag" {
-					query += "&tag=" + string(rest)
-				}
-            }
-        default:
-            r.Form = make(map[string][]string)
-			err = os.NewError("unknown Content-Type")
-			return err
-        }
-    }
-    r.Form, err = http.ParseQuery(query)
-    return
 }
 
 func renderTemplate(c *http.Conn, tmpl string, p *page) {
@@ -296,64 +184,105 @@ func uploadHandler(c *http.Conn, r *http.Request, urlpath string) {
 	p := newPage("", nil)
 	tag := ""
 	filepath := ""
-	upload := make([]byte, maxupload)
-	r2 := NewRequest2(r, upload)
-	err := r2.ParseForm()
-	if err != nil {
-		http.Error(c, err.String(), http.StatusInternalServerError)
-		return
-	}
 
-	// if "upload" is in the form, we got a new file, so write it to disk
-	for k, v := range (*r).Form {
-		if k == "upload" {
-			// write file in dir with YYYY-MM-DD format
-			filedir := path.Join(*picsdir, time.UTC().Format("2006-01-02"))
-			err = mkdir(filedir)
+	reader, err := r.MultipartReader()
+
+	// do nothing if no form	
+	if err == nil {
+		for {
+			part, err := reader.NextPart()
 			if err != nil {
 				http.Error(c, err.String(), http.StatusInternalServerError)
 				return
 			}
-			// create thumbsdir while we're at it
-			err = mkdir(path.Join(filedir, thumbsDir))
-			if err != nil {
-				http.Error(c, err.String(), http.StatusInternalServerError)
-				return
-			}
-			// finally write the file
-			filepath = path.Join(filedir, v[0])
-			err := ioutil.WriteFile(filepath, r2.upload, 0644)
-			if err != nil {
-				http.Error(c, err.String(), http.StatusInternalServerError)
-				return
-			}
-			p.title = v[0] + ": upload sucessfull"
-			if tag != "" {
+			if part == nil {
 				break
 			}
-			continue
-    	}
-		if k == "tag" {
-			tag = v[0]
-			if p.title != "" {
-				break;
+			partName := part.FormName()
+			// get the file
+			if partName == fileinform {
+				// get the filename 
+				var filename string
+				for k, v := range part.Header {
+					if k == "Content-Disposition" {
+						filename = v[strings.Index(v, "filename="):]
+						filename = filename[10:len(filename)-1]
+					}
+				}
+				// get the upload
+				b := make([]byte, maxupload)
+				var upload []byte
+				for {
+					n, err := part.Read(b)
+					if err != nil {
+						if err != os.EOF {
+//TODO: not sure that actually detects an unexpected EOF, oh well...
+							http.Error(c, err.String(), http.StatusInternalServerError)
+							return 
+						}
+						break
+					}	
+					upload = bytes.Add(upload, b[0:n])
+					if len(upload) > maxupload {
+						err = os.NewError("upload too large")
+						http.Error(c, err.String(), http.StatusInternalServerError)
+						return
+					}
+				}				
+				// write file in dir with YYYY-MM-DD format
+				filedir := path.Join(*picsdir, time.UTC().Format("2006-01-02"))
+				err = mkdir(filedir)
+				if err != nil {
+					http.Error(c, err.String(), http.StatusInternalServerError)
+					return
+				}
+				// create thumbsdir while we're at it
+				err = mkdir(path.Join(filedir, thumbsDir))
+				if err != nil {
+					http.Error(c, err.String(), http.StatusInternalServerError)
+					return
+				}
+				// finally write the file
+				filepath = path.Join(filedir, filename)
+				err = ioutil.WriteFile(filepath, upload, 0644)
+				if err != nil {
+					http.Error(c, err.String(), http.StatusInternalServerError)
+					return
+				}
+				p.title = filename + ": upload sucessfull"
+				if tag != "" {
+					break
+				}
+				continue
+			}
+			// get the tag
+			if partName == taginform {
+				b := make([]byte, 128)
+				n, err := part.Read(b)
+//TODO: better err handling ?
+				if err != nil {
+					b = b[0:n]
+					tag = string(b)
+				}
+				if p.title != "" {
+					break;
+				}
+			}
+		}
+		// only insert tag if we have an upload of a pic and a tag for it			
+		if tag != "" && p.title != "" {
+			if titleValidator.MatchString(tag) && 
+				picValidator.MatchString(filepath) {
+				err = mkThumb(filepath)
+				if err != nil {
+					http.Error(c, err.String(), http.StatusInternalServerError)
+					return 
+				}
+				insert(filepath[rootdirlen+1:], tag)
 			}
 		}
 	}
-
-	// only insert tag if we have an upload of a pic and a tag for it			
-	if tag != "" && p.title != "" {
-		if titleValidator.MatchString(tag) && 
-			picValidator.MatchString(filepath) {
-			err = mkThumb(filepath)
-			if err != nil {
-				http.Error(c, err.String(), http.StatusInternalServerError)
-				return 
-			}
-			insert(filepath[rootdirlen+1:], tag)
-		}
-	}
-
+		
 	renderTemplate(c, "upload", p)
 }
 
